9e93a993fadbe276465860850eb97228480e0fef
rayleigh420 <nhatduy0409@gmail.com>
2023-06-28 09:47:45 +0700

    Use jquery to collapse and expand footer

--- assest/javascripts/custom.js
+++ assest/javascripts/custom.js
@@ -1,6 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var myModal = new bootstrap.Modal(document.getElementById("modal"), {
-        keyboard: false
+// const toggleIcons = document.querySelectorAll('.toggle-icon');
+// const contents = document.querySelectorAll('.content');
+
+// toggleIcons.forEach((toggleIcon, index) => {
+//     toggleIcon.addEventListener('click', () => {
+//         if (contents[index].classList.contains('d-none')) {
+//             contents[index].classList.remove('d-none');
+//             toggleIcon.textContent = '-';
+//         } else {
+//             contents[index].classList.add('d-none');
+//             toggleIcon.textContent = '+';
+//         }
+//     });
+// });
+
+$(document).ready(function () {
+    $('.toggle-icon').on('click', function () {
+        var $content = $(this).closest('.footer_info').find('.content');
+        if ($content.hasClass('d-none')) {
+            $content.removeClass('d-none');
+            $(this).text('-');
+        } else {
+            $content.addClass('d-none');
+            $(this).text('+');
+        }
     });
-    myModal.show();
 });
